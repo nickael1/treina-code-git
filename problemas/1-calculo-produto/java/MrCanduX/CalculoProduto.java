f259bebb0c40c2bf32c2a5adabd18bab00bc29dc
MrCanduX <igorminerva81@gmail.com>
2023-04-22 15:18:19 -0300

    resolvendo o problema 1 em java

---
+++ problemas/1-calculo-produto/java/MrCanduX/CalculoProduto.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class CalculoProduto {
+
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("escreve o ID do produto: ");
+        int id = input.nextInt();
+        System.out.println("escreve a quantidade do produto: ");
+        int quantidade = input.nextInt();
+        System.out.println("escreve o valor unitario: ");
+        float valorUnitario = input.nextFloat();
+
+        float valorPago = valorUnitario * quantidade;
+
+        System.out.println("O produto "+id+" custa R$: "+valorPago);
+
+        
+    }
+}
+  
