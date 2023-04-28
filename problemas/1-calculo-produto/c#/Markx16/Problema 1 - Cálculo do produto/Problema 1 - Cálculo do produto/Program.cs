f0f894efcd6be1e88a9863902cd9c355d6bb1ba0
Markx16 <62144006+Markx16@users.noreply.github.com>
2023-04-27 21:51:43 -0300

    feat: resolvendo o desafio 1

---
+++ problemas/1-calculo-produto/c#/Markx16/Problema 1 - Cálculo do produto/Problema 1 - Cálculo do produto/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Problema_1___Cálculo_do_produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int validacao;
+            do
+            {
+                Console.Clear();
+
+                // Entrada de dados
+                Console.WriteLine("---Digite as informações---");
+                Console.Write("Código do produto: ");
+                int codigo = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Quantidade: ");
+                int quantidade = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Valor unitário: ");
+                double valorUnitario = Convert.ToDouble(Console.ReadLine());
+
+                // Cálculo do valor da compra
+                double calculoCompra = valorUnitario * quantidade;
+
+                // - Método responsável por arredondar para o inteiro mais proximo.
+                var valor = Math.Round(calculoCompra, 2);
+
+                // Saída das informações
+                Console.Clear();
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine($"O produto ID {codigo} custa R$ {valor}");
+                Console.WriteLine("-----------------------------------------");
+
+                Console.WriteLine("\nDigite '0' para encerrar ou qualquer número inteiro para continuar.");
+                validacao = Convert.ToInt32(Console.ReadLine());
+
+            } while (validacao != 0);
+
+
+        }
+    }
+}
--- problemas/1-calculo-produto/c#/Markx16/ficha-modelo.md
+++ problemas/1-calculo-produto/c#/Markx16/ficha-modelo.md
@@ -2,11 +2,11 @@
 
 **Exercicio:** 1 - Cálculo Simples
 
-**Nome:** username  e nome completo aqui
+**Nome:** Markx16  e Markswell
 
 **Comentários:** Aqui informe uma explicação de como resolveu o problema e o que achou da descrição do problema, comentários, etc. É opcional.
 
-**Como rodar a solução**: 
+**Como rodar a solução**: Dentro do Visual Studio clicar no botão 'Iniciar sem depurar'
 
 Use o comando abaixo: 
 ```bash
