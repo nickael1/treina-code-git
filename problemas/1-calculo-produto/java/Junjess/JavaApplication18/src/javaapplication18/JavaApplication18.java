828d3f1e9606ba99a851ea4140d9c4c780f6a68f
Rafaela <rafaelajung12@gmail.com>
2024-04-19 10:33:43 -0300

    feat: resolução do problema número 1. Contém um arquivo .java e um arquivo .md. O arquivo .java foi feito no IDE NetBeans

---
+++ problemas/1-calculo-produto/java/Junjess/JavaApplication18/src/javaapplication18/JavaApplication18.java
@@ -0,0 +1,26 @@
+
+package javaapplication18;
+
+import java.util.Scanner;
+
+public class JavaApplication18 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Digite o código do produto: ");
+        int cod = Integer.parseInt(input.nextLine());
+        
+        System.out.print("Digite a quantidade: ");
+        int quantidade = Integer.parseInt(input.nextLine());
+        
+        System.out.print("Digite o valor unitário: ");
+        float valorUnit = Float.parseFloat(input.nextLine());
+        
+        float valorTotal = valorUnit * quantidade; 
+        
+        System.out.println("O produto ID "+cod+" custa R$: "+valorTotal);
+    }
+    
+}
